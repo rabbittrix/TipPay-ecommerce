d22d3f44ca338e6dbf4691adb42a121fbdc990e7
rabbittrix <rabbittrix@hotmail.com>
2023-11-27 19:42:11 +0000

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dashboard',
-  description: 'E-Commerce Dashboard',
+  title: 'TipPay Dashboard',
+  description: 'TipPay Store Dashboard',
 }
 
 export default async function RootLayout({
